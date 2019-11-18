3c2c27b7068def60309f9af55db6a27031addc84
Andy Nguyen <apnguyen11@yahoo.com>
2019-11-18 16:10:19 -0600

    added decrement button and bootstrap styling

--- index.js
+++ index.js
@@ -6,19 +6,24 @@ class App extends React.Component{
         this.state.counter ++
         this.setState({})
     }
+    decrementBtn = () => {
+        this.state.counter --
+        this.setState({})
+    }
     render(){
         return(
-            <CounterDisplay counter={this.state.counter} action={this.incrementBtn}/>
+            <CounterDisplay counter={this.state.counter} action1={this.incrementBtn} action2={this.decrementBtn}/>
         )
     }
 }
 
 const CounterDisplay = function(props){
-    console.log(props.counter)
+    console.log(props)
     return (
-        <div>
+        <div className="d-flex flex-column justify-content-center align-items-center m-auto rounded" style={{width:"200px", height:"300px", backgroundColor:"#f4f4f4", border:"5px solid black"}}>
             <h1>{props.counter}</h1>
-            <button onClick={props.action}>Increment</button>
+            <button onClick={props.action1}  className="button btn-success btn-lg m-1">Increment</button>
+            <button onClick={props.action2}  className="button btn-success btn-lg m-1">Decrement</button>
         </div>
         
     )
